1f70474707aa66407406e38dcd9c61a74e4c8811
Oryanick <fosokengoryanick@gmail.com>
2026-05-22 10:46:04 +0200

    feat: transaction examples for safe lending operations

---
+++ lend.sql
@@ -0,0 +1,19 @@
+PRAGMA foreign_keys = ON;
+
+BEGIN;
+
+--  Verfügbarkeit Check 
+SELECT COUNT(*) AS open_loans
+FROM ausleihe
+WHERE exemplar_id = 5
+AND rueckgabe_datum IS NULL;
+
+-- ausleihe einfügen 
+INSERT INTO ausleihe (ausleihe_id, exemplar_id, mitglied_id, ausleihe_datum)
+VALUES (5, 5, 3, CURRENT_DATE);
+
+COMMIT;
+
+-- Kontrolle
+
+SELECT * FROM ausleihe WHERE ausleihe_id = 5;
